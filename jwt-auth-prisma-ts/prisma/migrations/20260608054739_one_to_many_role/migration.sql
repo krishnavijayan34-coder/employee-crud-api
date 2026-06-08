334accad372f461761fb84394e419d0606b3c722
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-06-08 11:48:19 +0530

    one to many

---
+++ jwt-auth-prisma-ts/prisma/migrations/20260608054739_one_to_many_role/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `userrole` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `roleId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `userrole` DROP FOREIGN KEY `UserRole_roleId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `userrole` DROP FOREIGN KEY `UserRole_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `role` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `roleId` INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE `userrole`;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
